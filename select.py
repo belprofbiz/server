cc3a7e8d3135ea0b6b08d4618298816a69ee7885
belprofbiz <belprofbiz@yandex.ru>
2020-09-01 15:26:16 +0300

    Изменение названий

--- select.py
+++ select.py
@@ -2,7 +2,7 @@ import mysql.connector #Importing Connector package
 mysqldb=mysql.connector.connect(host="localhost",user="belprofbiz",password="ANANERBE_80224552332")#established connection   
 mycursor=mysqldb.cursor()#cursor() method create a cursor object  
 mycursor.execute ("USE andrey")
-mycursor.execute("select * from andrey")#Execute SQL Query to select all record
+mycursor.execute("select * from newtable")#Execute SQL Query to select all record
 
     
 mysqldb.close()#Connection Close
